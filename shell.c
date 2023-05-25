1c62ccf1f4d767cf373d246be4428df958a9613d
dan1bayomi <dan234bayomi@gmail.com>
2023-05-24 18:30:01 -0700

    Fixing errors

--- shell.c
+++ shell.c
@@ -37,6 +37,27 @@ void free_all(char **args, char *input, char *path)
 }
 
 /**
+ * run_command - performs execution on command
+ * @input_args: return from get_args
+ * @input: Input string
+ * @command: Command to be made
+ */
+void run_command(char **input_args, char *input, char *command)
+{
+	command = get_path(input_args[0], env);
+	if (command == NULL)
+		handle_cmd_error(input_args, input);
+
+	input_args[0] = command; /* changing args */
+
+	/* Run command */
+	execve(input_args[0], input_args, NULL);
+	free_all(input_args, input, command);
+	perror("./hsh");
+	exit(EXIT_FAILURE);
+}
+
+/**
  * main - Main shell container
  * @ac: No of arguments
  * @av: Argument list
@@ -44,41 +65,36 @@ void free_all(char **args, char *input, char *path)
  *
  * Return: 0
  */
-int main(int ac, char *av[], char *env[])
+int main(int ac, char *av[] __attribute__((unused)), char *env[])
 {
-	char *input, *command, **input_args;
+	char *input = NULL, *command = NULL, **input_args = NULL;
 	int status, ret = 0;
 	pid_t pid;
+	int (*builtin_func)(void);
 
+	(void)ac;
 	while (1)
 	{
 		input = get_command();
-		if (input == "")
+		if (input[0] == '\0')
 			continue;
 		if (input == NULL) /* Checking NULL condition */
 			exit(EXIT_SUCCESS);
 
 		input_args = get_args(input, " "); /* splitting the input */
-		ret = get_builtin(input_args[0])(); /* builtins checked */
-		if (ret == -1)
-			handle_cmd_error(input_args, input);
+		builtin_func = get_builtin(input_args[0]); /* builtins checked */
+		if (builtin_func != NULL)
+		{
+			ret = builtin_func();
+			if (ret == -1)
+				handle_cmd_error(input_args, input);
+		}
 
 		pid = fork();
 		if (pid == -1) /* Error in pid */
 			handle_cmd_error(input_args, input);
 		else if (pid == 0)
-		{
-			/* Child process */
-			command = get_path(input_args[0], env);
-			if (command == NULL)
-				handle_cmd_error(input_args, input);
-			input_args[0] = command; /* changing args */
-			/* Run command */
-			execve(input_args[0], input_args, NULL);
-			free_all(input_args, input, command);
-			perror("./hsh");
-			exit(EXIT_FAILURE);
-		}
+			run_command(input_args, input, command);
 		else
 			wait(&status);
 		free_all(input_args, input, command);
